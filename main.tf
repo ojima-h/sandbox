28923ed22d8f0c15b20c3c0a01962f8d2f2afcb6
Ojima Hikaru <hikaru.ojima@mixi.co.jp>
2021-06-14 13:52:07 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 resource "local_file" "foo" {
-    content     = "foooooooooooooooooooooo!\n${var.name}\n${terraform.workspace}"
+    content     = "fooooooooooooooooooooooo!\n${var.name}\n${terraform.workspace}"
     filename = "${path.module}/foo.bar"
 }
